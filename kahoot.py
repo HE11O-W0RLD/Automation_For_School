f60cd9c55ba238b8955df5de6042ac6cb93a5e36
HE11O-W0RLD <nightknight12345678@gmail.com>
2023-06-21 14:28:47 +0300

    Changed message in kahoot

--- kahoot.py
+++ kahoot.py
@@ -1,1 +1,1 @@
-#I wasnt able find any patterns that indicate the right answer nor how to get the id of the quiz via just the id... sorry.
+#I wasnt able find any patterns that indicate the right answer nor how to get the id of the quiz via just the participation number... sorry.
